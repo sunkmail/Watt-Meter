f6faedd1bfab4a86240458381aceca2dce8e290b
Scott <sunkmail@gmail.com>
2016-06-30 20:10:37 -0700

    Update Watt_Meter_Arduino.ino

--- Watt_Meter_Arduino.ino
+++ Watt_Meter_Arduino.ino
@@ -2,7 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 20 chars and 4 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 20 chars by 4 line display
 
 Adafruit_ADS1115 ads(0x48);       // Create an instance of the ADC  (With ADDR line to GND, address is 0x48 - Default))
 int16_t adc0, adc1, adc2, adc3;   // Setup ADC output variables - signed integers of exactly 16 bits
@@ -13,6 +13,14 @@ boolean Relay_On = false;
 const byte LtBut = 10;          // Pin #'s for menu control Buttons
 const byte RtBut = 11;
 
+float Vrms = 0;
+float Iave = 0;
+float Imax = 0;
+float kWh = 0;
+float Watts = 0;
+float RunTime;
+float Hz;
+
 
 
 void setup() {
@@ -22,27 +30,40 @@ void setup() {
 
   pinMode(LtBut, INPUT_PULLUP); // Pin setup for menu control Buttons
   pinMode(RtBut, INPUT_PULLUP);
-  
+
   lcd.init();             // initialize the lcd
   lcd.init();             // Not sure why it's done twice, but is this way in examples
   lcd.backlight();        // turn on backlight
-  LCDLayout();            // setup static Display elements
+  LCDHomeMenuLayout();    // setup initial static Display elements
 
   ads.begin();            // Initialize ads1115
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
 
-void LCDLayout()
+void LCDHomeMenuLayout()
 {
+    // Data Display from Previous Test
   lcd.setCursor(0, 0);      // move cursor to first position (0) of the Top (0) Line
-  lcd.print("Current Mode: ");
-  lcd.setCursor(12, 0);
-  lcd.print("Vrms:");
-  lcd.setCursor(9, 1);
-  lcd.print("Countdown: ");
-  
+  lcd.print("V: XXX.xx");
+  lcd.setCursor(10, 0);
+  lcd.print("Iave XX.xx");
+  lcd.setCursor(0, 1);
+  lcd.print("kWh XXX.xx");
+  lcd.setCursor(11, 1);
+  lcd.print("Imx XX.xx");
+
+    // Next Run Info
+  lcd.setCursor(0, 2);
+  lcd.print("Next Run Timer: ");   // Follow with
+
+    // Main Menu buttons
+  lcd.setCursor(0, 3);
+  lcd.print("START");
+  lcd.setCursor(14, 3);
+  lcd.print("ADJUST");
+
 }
